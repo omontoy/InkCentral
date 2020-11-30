4e98be9e3961ed638d4789bad642258198db444c
Ivan Gonzalez <gonzalez.ivand@gmail.com>
2020-11-30 11:30:40 -0500

    Improve artist card design (#16)
    * Improve artist card design
    
    * Remove comment line on artist.js file

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,27 +1,30 @@
 import Card from 'react-bootstrap/Card'
-import ListGroup from 'react-bootstrap/ListGroup'
 
 
 export function Artist ({ name, nickname, location, email, phone,image }) {
   return (
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={image} />
+      <Card  
+        bg="dark" 
+        text="light" 
+        className='artistCards'
+        style={{ borderRadius: "25px" }}
+      >
+        <Card.Img 
+          variant="bottom" 
+          src={image} 
+          className="cardImage"
+          style={{ borderRadius: "25px", borderBottomLeftRadius: "0px", borderBottomRightRadius:"0px" }}
+        />
         <Card.Body>
           <Card.Title>{name}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">{nickname}</Card.Subtitle>
-          
-          <ListGroup variant="flush">
-            <ListGroup.Item>{location}</ListGroup.Item>
-            <ListGroup.Item>{email}</ListGroup.Item>
-            <ListGroup.Item>{phone}</ListGroup.Item>
-          </ListGroup>
-          
-          <Card.Link href="#">Go Back</Card.Link>
-          <Card.Link href="#">See more</Card.Link>
+          <Card.Text>{email}</Card.Text>
         </Card.Body>
-      </Card>     
+      </Card>  
   )
 }
+
+
 
 
 
