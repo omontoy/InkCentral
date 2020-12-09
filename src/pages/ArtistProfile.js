5f11bc791c198b7b650ebfa7c7c10c23a5de21fc
Ivan Gonzalez <gonzalez.ivand@gmail.com>
2020-12-08 20:25:38 -0500

    Improve style and appearance of the artist profile view (#24)

--- src/pages/ArtistProfile.js
+++ src/pages/ArtistProfile.js
@@ -1,13 +1,17 @@
 import { useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getArtist } from '../store/artistReducer';
+import Card from 'react-bootstrap/Card';
+import CardColumns from 'react-bootstrap/CardColumns';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container'
 
 
 
 
 function ArtistProfile() {
-  let history = useHistory()
   let { artistId } = useParams();
   const dispatch = useDispatch()
   const { artist, error } = useSelector(
@@ -16,7 +20,7 @@ function ArtistProfile() {
     })
   useEffect(()=>{
     dispatch(getArtist(artistId))
-  }, []);
+  }, [dispatch, artistId ]);
   
   if(error) return <h1 className="main">
                       Ooops! Something went wrong with the chosen artist data request!!
@@ -24,20 +28,60 @@ function ArtistProfile() {
                     </h1>
     
   return(
-    <div className='main'>
-      <ul>
-        <li>{artist.email}</li>
-        <li>{artist.name}</li>
-        <li>{artist.nickname}</li>
-        <li>{artist.location}</li>
-        <li>{artist.updatedAt}</li>
-        <li>{artist.home}</li>
-        <li>
-          <img src={artist.image} alt='tattoo example'/>
-        </li>
-      </ul>
+    <div className='artistProfileContainer'>
+      <Jumbotron className="jumbo" fluid >
+        <Container>
+          <h1>{artist.nickname}</h1>
+          <p>This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        </Container>
+      </Jumbotron>
+      
+      <CardColumns>
+        <Card className="bg-dark text-white">
+          <Card.Img src={artist.image} className="artistProfileImage" alt="main tattoo" />
+          <Card.ImgOverlay>
+            <Card.Title className="imgTitle">{artist.nickname}</Card.Title>
+          </Card.ImgOverlay>
+        </Card>
+        <Card className="p-3">
+          <blockquote className="blockquote mb-0 card-body">
+            <p><em>The human body is the ultimate canvas...</em></p>
+            <footer className="blockquote-footer">
+              <small className="text-muted">
+                {artist.name} 
+              </small>
+            </footer>
+          </blockquote>
+        </Card>
+        <Card className="p-6 cardTitle" >
+          <Card.Body>
+            <Card.Title>{artist.name}</Card.Title>
+            <Card.Text>
+              Check my work out through social media too with the following links
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item><i class="fab fa-2x fa-instagram"></i></ListGroup.Item>
+            <ListGroup.Item><i class="fab fa-2x fa-facebook"></i></ListGroup.Item>
+            <ListGroup.Item><i class="fab fa-2x fa-linkedin"></i></ListGroup.Item>
+            <ListGroup.Item><i class="fab fa-2x fa-discord"></i></ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="#">Contact</Card.Link>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+          <Card.Title className="cardTitle">Artist Information </Card.Title>
+            <ListGroup variant="flush">
+              <ListGroup.Item>Phone: {artist.phone}</ListGroup.Item>
+              <ListGroup.Item>Located at: {artist.location}</ListGroup.Item>
+              <ListGroup.Item>Email me at: {artist.email}</ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+      </CardColumns>
     </div>
   )
-
 }
 export default ArtistProfile;
