277ae9730aacf01190263858ce8ca9caf540c9ea
Orlando Montoya <omb0320@gmail.com>
2020-12-11 00:34:33 -0500

    Add the client reducer to the index store

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,14 @@ import thunk from 'redux-thunk'
 import artistReducer from './artistReducer'
 import registerReducer from './registerReducer'
 import loginReducer from './loginReducer'
+import clientReducer from './clientReducer'
 
-const reducers = combineReducers({ 
+
+const reducers = combineReducers({
   artistReducer,
   registerReducer,
-  loginReducer 
+  loginReducer,
+  clientReducer
 })
 const middlewares = applyMiddleware(thunk)
 
