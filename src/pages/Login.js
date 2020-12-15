3b3d104dec74b2c4edfb380018143f7392b33f70
Orlando Montoya <omb0320@gmail.com>
2020-12-15 11:20:16 -0500

    Remove dispatch from useEffect

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,7 +7,12 @@ import Col from 'react-bootstrap/Col'
 import '../App.css';
 import swal from 'sweetalert';
 import { useHistory } from "react-router-dom"
-import { artistLogin, clientLogin, cleanuperror, cleanLogin } from '../store/loginReducer';
+import {
+  artistLogin,
+  clientLogin,
+  cleanuperror,
+  cleanLogin
+} from '../store/loginReducer';
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 
@@ -19,32 +24,35 @@ export function Login(){
       return { login, loading, errorLog }
     }
   )
+
   useEffect(() => {
     if(errorLog){
       swal("Sorry!!",`${ errorLog.message }`,"error")
       dispatch( cleanuperror( errorLog ) )
     }
   },[errorLog])
+
   useEffect(() => {
     const token = localStorage.getItem('token')
     if(token){
       swal("Welcome!!",`${ email }`,"success")
-      dispatch(cleanLogin())
       history.push('/')
     }
   },[login])
-  
+
   const [ loginForm, setLoginForm ] = useState({
     email: "",
     userType: "Client",
     password: "",
   })
+
   const handleChange = e => {
     setLoginForm({
       ...loginForm,
       [e.target.name]: e.target.value
     })
   }
+
   const handleLogin = async e => {
     e.preventDefault();
     const { email, password, userType } = loginForm;
@@ -54,65 +62,72 @@ export function Login(){
       dispatch(artistLogin(email, password))
     }
   }
+
   const { email, password } = loginForm
+
   return(
     <div className="main">
       <Container >
         <Row className="justify-content-md-center">
-          <Col md="4"  >
-          <Form className="loginForm" onSubmit={handleLogin}>
-            <h1>Login</h1>
-            <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              name="email"
-              value={email}
-              required
-              onChange={handleChange}
-            />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={password}
-              required
-              onChange={handleChange}
-            />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckBox" className="userTypeRadio" >
-              <Form.Check
-                type="radio"
-                label="Artist"
-                name="userType"
-                value="Artist"
-                onChange={handleChange}/>
-              <Form.Check
-                type="radio"
-                label="Client"
-                name="userType"
-                value="Client"
-                defaultChecked
-                onChange={handleChange}/>
-            </Form.Group>
-            <Button
-              className="form-control"
-              variant="primary"
-              type="submit"
-              disabled= {loading}> Login </Button>
-            <Button variant="link">Forgot your password?</Button>
-            <br></br>
-            <br></br>
-            <hr></hr>
-            <LinkContainer to="/register" variant="dark">
-              <Button>Register</Button>
-            </LinkContainer>
-            </Form>
+          <Col md="4">
+            <Form className="loginForm" onSubmit={handleLogin}>
+              <h1>Login</h1>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                  value={email}
+                  required
+                  onChange={handleChange}
+                />
+              </Form.Group>
 
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  required
+                  onChange={handleChange}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicCheckBox" className="userTypeRadio" >
+                <Form.Check
+                  type="radio"
+                  label="Artist"
+                  name="userType"
+                  value="Artist"
+                  onChange={handleChange}/>
+                <Form.Check
+                  type="radio"
+                  label="Client"
+                  name="userType"
+                  value="Client"
+                  defaultChecked
+                  onChange={handleChange}/>
+              </Form.Group>
+
+              <Button
+                className="form-control"
+                variant="primary"
+                type="submit"
+                disabled={loading}
+                >Login
+              </Button>
+
+              <Button variant="link">Forgot your password?</Button>
+              <br></br>
+              <br></br>
+              <hr></hr>
+              <LinkContainer to="/register" variant="dark">
+                <Button>Register</Button>
+              </LinkContainer>
+            </Form>
           </Col>
         </Row>
       </Container>
