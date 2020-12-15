5f9086c5fe84c787e15a1dfce78a842768cccc98
Orlando Montoya <omb0320@gmail.com>
2020-12-15 11:26:31 -0500

    Add one link to update artist or client user form

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,23 +8,39 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { cleanLogin } from '../store/loginReducer'
+import { resetUserType } from '../store/registerReducer'
+import { logoutArtist } from '../store/artistReducer'
+import { logoutClient } from '../store/clientReducer'
 
 export function Navigation() {
   const history = useHistory()
   const dispatch = useDispatch()
   const token = localStorage.getItem('token');
 
-  const { login } = useSelector(
-    ({ loginReducer: {login}}) => {
-      return { login }
+  const { login, userType } = useSelector(
+    ({ loginReducer: {login, userType}}) => {
+      return { login, userType }
     })
+
+  const { userTypeR, register } = useSelector(
+    ({ registerReducer: { userTypeR, register }}) => {
+        return { userTypeR, register }
+    })
+
   const handleClick = () => {
     localStorage.clear();
     dispatch( cleanLogin() )
+    dispatch( resetUserType() )
+    dispatch( logoutArtist() )
+    dispatch( logoutClient() )
   }
-  useEffect(() => {
-    history.push('/')
-  }, [login])
+
+  let user = null
+  if(!!userType) {
+    user = userType
+  } else {
+    user = userTypeR
+  }
 
   return(
     <Navbar fixed="top" bg="dark" variant="dark">
@@ -39,35 +55,31 @@ export function Navigation() {
 
         { token ? (
           <>
-          <LinkContainer to="/artistForm">
-            <Nav.Link>Artist Update</Nav.Link>
-          </LinkContainer>
+            <LinkContainer to={`/${user}Form`}>
+              <Nav.Link>User Update</Nav.Link>
+            </LinkContainer>
 
-          <LinkContainer to="/clientForm">
-            <Nav.Link>Client Update</Nav.Link>
-          </LinkContainer>
-
-          <LinkContainer to="/login" onClick={handleClick}>
-            <Nav.Link>Logout</Nav.Link>
-          </LinkContainer>
+            <LinkContainer to="/login" onClick={handleClick}>
+              <Nav.Link>Logout</Nav.Link>
+            </LinkContainer>
           </> ) :
           (
             <>
-            <LinkContainer to="/login">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
 
-            <LinkContainer to="/register">
-              <Nav.Link>Register</Nav.Link>
-            </LinkContainer>
+              <LinkContainer to="/register">
+                <Nav.Link>Register</Nav.Link>
+              </LinkContainer>
             </>
         )}
 
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
-      </Navbar>
-    )
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+    </Navbar>
+  )
 }
