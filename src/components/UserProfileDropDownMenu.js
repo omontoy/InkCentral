4eb5d43aa3890987e9aee1865f77fe64f10b0540
GiovannyRamirez <giovannyramirezs@hotmail.com>
2021-01-08 23:55:39 -0500

    Delete artist profile via redux

--- src/components/UserProfileDropDownMenu.js
+++ src/components/UserProfileDropDownMenu.js
@@ -2,62 +2,65 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Nav from 'react-bootstrap/Nav';
 import swal from 'sweetalert';
+import jwt_decode from "jwt-decode";
 import { LinkContainer } from 'react-router-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { getLoggedArtist } from '../store/artistReducer';
-import { getLoggedClient } from '../store/clientReducer';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { cleanLogin } from '../store/loginReducer';
+import { resetUserType } from '../store/registerReducer';
+import { logoutArtist, getLoggedArtist } from '../store/artistReducer';
+import { logoutClient, getLoggedClient } from '../store/clientReducer';
+import { deleteArtist } from '../store/actions/artist'
 
-
-export function UserProfileDropDownMenu({ user, handleLogOut }){
+export function UserProfileDropDownMenu({ user, handleLogOut }) {
   const dispatch = useDispatch();
 
-  function handleClick(){
-    if( user === "artist" ){
+  function handleClick() {
+    if (user === "artist") {
       dispatch(getLoggedArtist())
     }
     else {
       dispatch(getLoggedClient());
     }
-      
-  } 
+  }
 
-  const { artist } = useSelector(
-    ({ artistReducer: { artist } }) => {
-      return { artist }
-    })
+  const history = useHistory()
 
-  const { client } = useSelector(
-    ({ clientReducer: { client } }) => {
-      return { client }
-    })
+  const handleDelete = async () => {
 
-  const handleDelete = () => {
+    const token = sessionStorage.getItem('token')
+    const decoded = jwt_decode(token);
+
     swal({
       title: "Are you sure to delete your profile?",
       icon: "warning",
       buttons: true,
       dangerMode: true,
     })
-    .then(async(willDelete) => {
-      if(willDelete && user === 'client') {
-        await dispatch(getLoggedClient())
-        console.log(client)
-      } else if (willDelete && user === 'artist') {
-        await dispatch(getLoggedArtist())
-        console.log(artist)
-      } else {
-        swal("Your profile are safe here")
-      }
-    })    
+      .then(async (willDelete) => {
+        if (willDelete && user === 'client') {
+          console.log(decoded.id)
+        } else if (willDelete && user === 'artist') {
+          await dispatch(deleteArtist(decoded.id))
+          sessionStorage.clear();
+          await dispatch(cleanLogin())
+          await dispatch(resetUserType())
+          await dispatch(logoutArtist())
+          await dispatch(logoutClient())
+          history.push('/register')
+        } else {
+          swal("Your profile are safe here")
+        }        
+      })    
   }
 
-  return(
+  return (
     <DropdownButton
       menuAlign="right"
       title={<i className="fas fa-2x fa-user-astronaut"></i>}
       id="dropdown-menu-align-right"
       variant="dark"
-    > 
+    >
       <Dropdown.Item eventKey="1" >
         <LinkContainer to={`/${user}-form`} style={{ color: "black" }} >
           <Nav.Link><i className="fas fa-user-edit"></i>   Edit Profile</Nav.Link>
@@ -65,7 +68,7 @@ export function UserProfileDropDownMenu({ user, handleLogOut }){
       </Dropdown.Item>
       <Dropdown.Item eventKey="2">
         <LinkContainer to="/transactions" style={{ color: "black" }}  >
-          <Nav.Link onClick={ handleClick }><i className="fas fa-columns"></i>   Transactions</Nav.Link>
+          <Nav.Link onClick={handleClick}><i className="fas fa-columns"></i>   Transactions</Nav.Link>
         </LinkContainer>
       </Dropdown.Item>
       <Dropdown.Item eventKey="3">
@@ -75,17 +78,17 @@ export function UserProfileDropDownMenu({ user, handleLogOut }){
       </Dropdown.Item>
       <Dropdown.Divider />
       <Dropdown.Item eventKey="4"  >
-        <LinkContainer to="/login" onClick={ handleLogOut } style={{ color: "black" }}  >
+        <LinkContainer to="/login" onClick={handleLogOut} style={{ color: "black" }}  >
           <Nav.Link><i className="fas fa-sign-out-alt"></i>   Log Out</Nav.Link>
         </LinkContainer>
       </Dropdown.Item>
 
       <Dropdown.Divider />
-      <Dropdown.Item eventKey="5" onClick={ handleDelete }>
+      <Dropdown.Item eventKey="5" onClick={handleDelete}>
         <Nav.Link style={{ color: "red" }}>
           <i className="fas fa-user-minus"></i>   Delete Profile
         </Nav.Link>
       </Dropdown.Item>
-    </DropdownButton>           
+    </DropdownButton>
   )
 }
