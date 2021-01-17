0c9877c10456f3ededed14770b02d8bd8f2a1222
Ivan Gonzalez <gonzalez.ivand@gmail.com>
2021-01-17 13:14:21 -0500

    Complete GET and PUT requests to update password functionality

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -9,15 +9,19 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { inkCentralServer } from '../utils/apiaxios'
 import swal from 'sweetalert';
+import { useSelector } from 'react-redux';
+import { Loader } from '../components/Loader';
 
 
 
 export function ResetPassword(){
-  const [ isLoading, setIsloading ] = useState({ isLoading: true })
-  const [ email, setEmail ] = useState({ email: ''})
-  
+  const [ isLoading, setIsLoading ] = useState(true)
+  const [ email, setEmail ] = useState()
+  const [ password, setPassword ] = useState()
+  const [ error, setError ] =useState(null)
+  const [ isUpdated, setIsUpdated ] = useState(false)
+
   const { token } = useParams();
-  console.log(token);
   useEffect( async () => {
     try{
       const response = await inkCentralServer({
@@ -25,18 +29,120 @@ export function ResetPassword(){
         url:`/clients/reset/${token}`
       })
       const { data } = response.data;
-      // console.log(data.email)
       console.dir(data)
+      setEmail(data)
+      setIsLoading(false)
     }
     catch(error){
       console.dir(error);
+      setError(true)
     }
   }, []);
 
-  return(
+  const handleChange = e => {
+    // setPassword({
+    //   ...password,
+    //   [e.target.name]: e.target.value
+    // })
+    setPassword(e.target.value)
+  }
+
+  const updatePassword = async e => {
+    e.preventDefault();
+    console.log('password', password);
+    console.log('email', email);
+    try{
+      const response = await inkCentralServer({
+        method: 'PUT',
+        url: 'clients/updatePassword',
+        data: { 
+          email,
+          password
+        }
+      })
+      console.dir(response)
+      setIsUpdated(true)
+    }
+    catch (error) {
+      console.dir(error);
+    }
+  }
+
+  console.log(isLoading);
+  if(error){
+    return(
+      <div className="main">
+        <Form className="passwordResetForm">
+          <h4>Problem resetting password. Please request another link </h4>
+            <LinkContainer to='/forgot-password'>
+              <Button variant="link">Forgot your password?</Button>
+            </LinkContainer>
+            <LinkContainer to='/'>
+              <Button variant="link">Go Home</Button>
+            </LinkContainer>
+        </Form>
+      </div>
+    )
+  }  
+  if(isLoading) {
+    return (
+      <Container>
+        <h1 className="main"> Loading user data...</h1>
+        <Loader />
+      </Container>
+    )
+  }
+  return (
     <div className="main">
-      <h1>Reset Password</h1>
+      {!isUpdated ? 
+      <Container >
+        <Row className="justify-content-md-center">
+          <Col md="4">
+            <Form className="passwordResetForm" onSubmit= { updatePassword } >
+              <h1>Password Reset</h1>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter Password"
+                  name="password"
+                  required
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <Button
+                className="form-control"
+                variant="primary"
+                type="submit"
+                >Update Password
+              </Button>
+              <br></br>
+              <br></br>
+              <hr></hr>
+              <LinkContainer to="/register" variant="dark">
+                <Button>Register</Button>
+              </LinkContainer>
+            </Form>
+          </Col>
+        </Row>
+      </Container> :
+       <Container >
+       <Row className="justify-content-md-center">
+         <Col md="4">
+           <Form className="passwordResetSuccess" >
+             <p>Your password has been succesfully reset, please try login in again</p>
+             <br></br>
+             <br></br>
+             <hr></hr>
+             <LinkContainer to="/login" variant="primary">
+               <Button >Login</Button>
+             </LinkContainer>
+           </Form>
+         </Col>
+       </Row>
+     </Container> }
     </div>
   )
-
 }
+
+
