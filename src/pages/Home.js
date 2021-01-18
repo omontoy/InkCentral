568e77ad7a3f92749d6f21c9fa70ec0dd3ff2bbd
Orlando Montoya <omb0320@gmail.com>
2021-01-18 10:04:41 -0500

    Search bar (#57)
    * Add searchValue state to manage the input bar value
    
    * Create the onChange function to keep the input search bar value
    
    * Receive the searchValue from Navigation to dispatch getArtist function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,23 +6,27 @@ import { getArtists } from '../store/artistReducer'
 import { useSelector, useDispatch } from 'react-redux'
 import Container from 'react-bootstrap/Container';
 
+
 function Home() {
+
   const dispatch = useDispatch()
-  const { artists, loading, error } = useSelector(
-    ({ artistReducer: { artists, loading, error }}) => {
-      return { artists, loading, error }
+  const { artists, loading, error, searchValue } = useSelector(
+    ({ artistReducer: { artists, loading, error, searchValue }}) => {
+      return { artists, loading, error, searchValue }
     })
-  
+
   useEffect(() => {
-    dispatch(getArtists())
-  }, []);
+    dispatch(getArtists(searchValue))
+  }, [searchValue]);
 
   if (loading) return (
     <Container>
       <h1 className="main">Artists Data is still loading...</h1>
       <Loader/>
-    </Container>)    
+    </Container>)
   if (error) return <h1 className="main">Something went wrong with Artists Data</h1>
+  
+
   return (
     <div className="main">
       <Artists
